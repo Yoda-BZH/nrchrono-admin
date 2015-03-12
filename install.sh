c8adf3660d675aef4efe519e9c2090a0348fdd18
Yoda-BZH <yodabzh+gh@gmail.com>
2015-03-12 18:52:25 +0100

    extract archive only if we have downloaded it

--- install.sh
+++ install.sh
@@ -8,18 +8,18 @@ highcharts="Highcharts-4.1.3.zip"
 if [ ! -d web/highcharts ]
 then
     wget http://code.highcharts.com/zips/$highcharts
+    unzip -d web/highcharts $highcharts
 fi
 
-unzip -d web/highcharts $highcharts
 
 countdown="jquery.countdown.package-2.0.2.zip"
 
 if [ ! -d web/jqcountdown ]
 then
     wget http://keith-wood.name/zip/$countdown
+    unzip -d web/jqcountdown $countdown
 fi
 
-unzip -d web/jqcountdown $countdown
 
 
 if [ ! -d web/js/sprintf ]
