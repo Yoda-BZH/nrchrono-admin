897900344221a2a762c616d5845ad29324fdc00b
Yoda-BZH <yodabzh+gh@gmail.com>
2015-03-12 21:22:59 +0100

    css theme management

---
+++ src/AppBundle/Controller/CssController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+
+class CssController extends Controller
+{
+    private $list = array(
+            'default' => '',
+            'darkly'  => 'bundles/app/css/bootstrap.theme.darkly.min.css',
+            'slate'   => 'bundles/app/css/bootstrap.theme.slate.min.css',
+        );
+    /**
+     *
+     * @Route("/css/list", name="css_list")
+     * @Template("AppBundle:Css:index.html.twig")
+     */
+    public function indexAction()
+    {
+        return array(
+            'list' => array_keys($this->list),
+        );
+    }
+
+    /**
+     * @Route("/css/set/{name}", name="css_set")
+     */
+    public function setAction($name) {
+        if (!in_array($name, array_keys($this->list)))
+        {
+            return $this->redirect($this->generateUrl('homepage'));
+        }
+
+        $session = $this->getRequest()->getSession();
+        if ($name == 'default')
+        {
+            $url = null;
+        }
+        else
+        {
+            $url = $this->list[$name];
+        }
+
+        $session->set('theme', $url);
+
+        return $this->redirect($this->generateUrl('homepage'));
+    }
+}
