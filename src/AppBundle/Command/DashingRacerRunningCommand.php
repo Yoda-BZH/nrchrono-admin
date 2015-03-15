a4c5fe3b92954364d855e1a0d576d9367e61af55
Yoda-BZH <yodabzh+gh@gmail.com>
2015-03-15 14:08:14 +0100

    adding push to dashboard about who's running

---
+++ src/AppBundle/Command/DashingRacerRunningCommand.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace AppBundle\Command;
+
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+
+use Symfony\Component\DependencyInjection\Exception\ServiceNotFoundException;
+
+class DashingRacerRunningCommand extends ContainerAwareCommand
+{
+    protected function configure()
+    {
+        $this
+            ->setName('dashing:racer:running')
+            ->setDescription('Racers running')
+            //->addArgument('team', InputArgument::REQUIRED, 'team to use')
+        ;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $em = $this->getContainer()->get('doctrine')->getManager();
+
+        $repoTeam = $em->getRepository('AppBundle:Team');
+        $repoRacer = $em->getRepository('AppBundle:Racer');
+
+
+        $teams = $repoTeam->findAll();
+        $now = new \Datetime();
+        $nextGuesser = $this->getContainer()->get('racer.next');
+
+        foreach($teams as $team)
+        {
+            $nextRacer = $nextGuesser
+                ->setTeam($team)
+                ->getNext()
+                ;
+
+            if(!$nextRacer) {
+                $nextRacer = $repoRacer->getFirstOfTeam($team);
+            }
+
+            $racers[] = array('label' => $team->getName(), 'value' => $nextRacer->getNickname());
+        }
+
+
+        $url = 'http://localhost:3030/widgets/sur-la-piste';
+        $curl = curl_init($url);
+        curl_setopt($curl, CURLOPT_POST, true);
+        curl_setopt($curl, CURLOPT_POSTFIELDS, json_encode(array(
+            "auth_token" => "ttrtyuijk",
+            "items" => $racers,
+        )));
+        curl_exec($curl);
+
+        return 0;
+    }
+}
