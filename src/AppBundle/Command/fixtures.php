a2639a26913deae46480c7e2d58dd9248e8d20fa
Yoda-BZH <yodabzh+gh-f-f2b@gmail.com>
2016-05-26 13:36:19 +0200

    * add ficxtures injection

---
+++ src/AppBundle/Command/fixtures.php
@@ -0,0 +1,61 @@
+<?php
+
+$team1 = array(
+    array('Frederic'),
+    array('Jean-Yves'),
+    array('Nelly Pi'),
+    array('Vincent'),
+    array('Patrice'),
+    array('Marine'),
+    array('Régis'),
+    array('Stoph'),
+    array('Arnaud'),
+    array('Gérard'),
+);
+
+$team2 = array(
+    array('Morgane'),
+    array('Benoit'),
+    array('Cédric'), //    array('Guillaume'),
+    array('Patrick'),
+    array('Nelly Pe'),
+    array('Marco'),
+    array('Baptiste'),
+    array('Matthias'),
+    array('Fred'),
+    array('Alban'),
+);
+
+$team3 = array(
+    array('Alexandre'),
+    array('Philippe'),
+    array('Lise'),
+    array('Rémi'),
+    array('Cyrille'),
+    array('Laurent'),
+);
+
+$team4 = array(
+    array('Clémence'),
+    array('Véronique'),
+    array('Sophie'),
+    array('Myriam'),
+    array('Céline'),
+    array('Elodie'),
+);
+
+$team5 = array(
+    array('Yves'),
+    array('Guillaume'),
+    array('Sonia'),
+    array('Franck'),
+    array('Samuel'),
+);
+
+$teamsTypes = array(
+    array(10, 5, 5, $pausesTeam[10], '#DA1515', 'NR-Ptits LU', $team1),
+    array(10, 5, 5, $pausesTeam[10], '#BCDA15', 'NR-BN', $team2),
+    array(6, 3, 3, $pausesTeam[6], '#DA8715', 'NR-Princes', $team3),
+    array(6, 3, 3, $pausesTeam[6], '#DA1590', 'NR-Duchesses', $team4),
+    array(5, 3, 2, $pausesTeam[5], '#1586DA', 'NR-NART', $team5),
+);
