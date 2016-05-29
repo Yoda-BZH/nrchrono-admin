905399bb561cc37c3bd19f9d198a97d3d352b85e
Yoda-BZH <yodabzh+gh-f-f2b@gmail.com>
2016-05-29 16:43:30 +0200

    * re set fixtures du 3 minutes, add verbose for debug

--- reset.sh
+++ reset.sh
@@ -3,6 +3,6 @@
 sudo supervisorctl stop all
 app/console doctrine:schema:drop --force --env=prod
 app/console doctrine:schema:create --env=prod
-app/console faker:fixtures 12 --env=prod
+app/console faker:fixtures 3 --env=dev -v
 app/console faker:matsport:gen --env=prod
 sudo supervisorctl start all
